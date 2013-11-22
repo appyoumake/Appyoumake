bf8eee4168fdde6b4027c64c0be49c6594f72741
Arild <arild.bergh@ffi.no>
2013-11-22 13:56:59 +0100

    Removed Acme demo

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitd375f18c44628bb4317def4b6379de14::getLoader();
+return ComposerAutoloaderInitdbdad30ec7011defff23fd18cfb2aa65::getLoader();
