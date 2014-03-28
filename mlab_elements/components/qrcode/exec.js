f3267f8c06614b15f0e85c7fef68ef62aff23b0d
Arild Bergh <info@bergh.fm>
2014-03-28 13:22:34 +0100

    Fikset bug som gjorde at komponenter ikke kunne manipuleres (legge til link, qr kode, etc) etter at siden var lagret

---
+++ mlab_elements/components/qrcode/exec.js
@@ -0,0 +1,24 @@
+document.mlab_code_h2 = new function() {
+	
+	this.config = new Array();
+    
+//el = element this is initialising, config = global config from conf.txt
+	this.onLoad = function (el, config) {
+        this.config = config;
+        $(el).find("h2").attr("contenteditable", "true");
+        $(el).find("h2").bind("blur keyup paste copy cut mouseup", function() { if ($(this).text().trim() == "") { $(this).text("Add content"); } } ) ;
+    };
+    
+	this.onSave = function (el) {
+		$(el).find("h2").removeAttr("contenteditable");
+    };
+            
+    this.getContentSize = function (el) {
+        return $(el).find("h2").text().length;
+    };
+    
+    this.custom_generate_QR_code = function () {
+        $(el).qrcode({text: $(el).find("h2").text(), background: "#ffffff", foreground: "#000000", render : "table"});
+    }
+
+};
