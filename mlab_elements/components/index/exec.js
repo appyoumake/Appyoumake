c04bf38c69d3862d0a6962472e5c123badf1d58f
Arild Bergh <info@bergh.fm>
2014-09-15 12:57:32 +0200

    Removed debugger statement from previous commit

--- mlab_elements/components/index/exec.js
+++ mlab_elements/components/index/exec.js
@@ -3,13 +3,11 @@ document.mlab_code_index = new function() {
 	this.config = {};
     
     this.onCreate = function (el, config, designer, url) {
-        debugger;
         this.onLoad (el, config, designer, url);
     };
     
 //el = element this is initialising, config = global config from conf.txt
 	this.onLoad = function (el, config, designer, url) {
-        debugger;
         this.config = config;
     };
     
