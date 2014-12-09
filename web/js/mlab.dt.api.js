7c39c1b45622e72ccc303abfdc24614761bbe0be
Arild Bergh <info@bergh.fm>
2014-12-09 13:41:23 +0100

    Minor change to getGUID to prefix with mlab_

--- web/js/mlab.dt.api.js
+++ web/js/mlab.dt.api.js
@@ -124,12 +124,12 @@ Mlab_dt_api.prototype = {
     },
 
 /**
- * Creates a GUID that is rfc4122 version 4 compliant.
+ * Creates a unique ID starting with the prefix mlab_, followed by a rfc4122 version 4 compliant GUID. 
  * This is typically used to create an ID for a component that must not clash with any other IDs.
  * @returns {String}
  */
     getGUID : function () {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        return 'mlab_' + 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
             var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
             return v.toString(16);
         });
