f9e0364822f1a83b108d3ea822abb6d75a267256
bardreitanffi <bard.reitan@ffi>
2015-11-04 04:13:42 -0800

    Added error functions to ws.send in _minimal_websocket

--- _minimal_websocket/app.js
+++ _minimal_websocket/app.js
@@ -19,10 +19,10 @@ mlabServicesCallbackServer.on('connection', function(ws) {
     if (url_info[1] != 0) {
         console.log("Connection established from " + ws.upgradeReq.connection.remoteAddress + " (unique windows ID = " +  url_info[1] + ")");
         mlabEditorClients[url_info[1]] = ws;
-        ws.send('{"data": {"status": "connected"}}');
+        ws.send('{"data": {"status": "connected"}}', function(error){console.log(error);});
     } else {
         console.log("Temporary connection from " + ws.upgradeReq.connection.remoteAddress);
-        ws.send('{"data": {"status": "SUCCESS"}}');
+        ws.send('{"data": {"status": "SUCCESS"}}', function(error){console.log(error);});
     }
     
     ws.on('message', function(data, flags) {
@@ -30,7 +30,7 @@ mlabServicesCallbackServer.on('connection', function(ws) {
         if (typeof data == "string") {
             var objData = JSON.parse(data);
         } else if (typeof data == "undefined") {
-            ws.send('{"data": {"status": "ERROR", "error": "received empty string"}}');
+            ws.send('{"data": {"status": "ERROR", "error": "received empty string"}}', function(error){console.log(error);});
             console.log('ERR: received empty string');
             return;
         } else {
@@ -40,20 +40,20 @@ mlabServicesCallbackServer.on('connection', function(ws) {
         if (typeof objData.destination_id != "undefined" && typeof objData.data != "undefined" && typeof mlabEditorClients[objData.destination_id] != "undefined") {
             console.log('DATA: ' + JSON.stringify(objData.data));
             mlabEditorClients[objData.destination_id].send(JSON.stringify(objData.data));
-            ws.send('{"data": {"status": "SUCCESS"}}');
+            ws.send('{"data": {"status": "SUCCESS"}}', function(error){console.log(error);});
             console.log('SENT TO: ' + objData.destination_id);
         } else {
             if (typeof objData.destination_id == "undefined") {
                 console.log('No destination present, nothing sent');
-                ws.send('{"data": {"status": "ERROR", "error": "No destination present, nothing sent"}}');
+                ws.send('{"data": {"status": "ERROR", "error": "No destination present, nothing sent"}}', function(error){console.log(error);});
             }
             if (typeof objData.data == "undefined") {
                 console.log('No data payload present, nothing sent');
-                ws.send('{"data": {"status": "ERROR", "error": "No data payload present, nothing sent"}}');
+                ws.send('{"data": {"status": "ERROR", "error": "No data payload present, nothing sent"}}', function(error){console.log(error);});
             }
             if (typeof mlabEditorClients[objData.destination_id] == "undefined") {
                 console.log('Mlab client ' + objData.destination_id + ' not connected');
-                ws.send('{"data": {"status": "ERROR", "error": "Mlab client ' + objData.destination_id + ' not connected"}}');
+                ws.send('{"data": {"status": "ERROR", "error": "Mlab client ' + objData.destination_id + ' not connected"}}', function(error){console.log(error);});
             }
         }
     });
