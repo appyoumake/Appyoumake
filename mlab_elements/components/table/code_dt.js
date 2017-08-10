52f28c51ec4752ec8bbf70c9517efafd60cfbb2a
Arild Bergh <info@bergh.fm>
2017-08-10 14:19:42 +0200

    MOre on table component

--- mlab_elements/components/table/code_dt.js
+++ mlab_elements/components/table/code_dt.js
@@ -8,10 +8,8 @@
     
 //el = element this is initialising, config = global config from conf.yml
 	this.onLoad = function (el) {
-        $(el).find("td").attr("contenteditable", "true");
-        $(el).find("td").bind("blur keyup paste copy cut mouseup", function() { if ($(this).text().trim() == "") { $(this).html("Add content"); } } ) ;
-        $('[contenteditable="true"]').focus(function(){
-            $(this).parents('table').find("[data-mlab-dt-table-focus='1']").removeAttr('data-mlab-dt-table-focus');
+        $(el).find("th, td").attr("contenteditable", "true").focus(function(){
+            $(this).closest('table').find("[data-mlab-dt-table-focus='1']").removeAttr('data-mlab-dt-table-focus');
             $(this).attr("data-mlab-dt-table-focus", 1);
         });
     };
@@ -48,21 +46,50 @@
     };
     
     this.custom_add_row = function (el) {
-		var clone = $(el).find("[data-mlab-dt-table-focus='1']").parent().clone(true);
-        clone.find("td").removeAttr('data-mlab-dt-table-focus');
-        clone.find('[contenteditable="true"]').focus(function(){
+        var table = this.getHTMLElement(el);
+		var row = table.find("[data-mlab-dt-table-focus='1']").parent();
+        var num_cells = row.find("td").length;
+        
+        var new_row = $(this.config.custom.row_start_html + Array(num_cells + 1).join(this.config.custom.cell_html) + "</tr>");
+        
+        new_row.find('[contenteditable="true"]').focus(function() {
             $(this).parents('table').find("[data-mlab-dt-table-focus='1']").removeAttr('data-mlab-dt-table-focus');
             $(this).attr("data-mlab-dt-table-focus", 1);
         });
-        $(el).find("[data-mlab-dt-table-focus='1']").parent().after(clone);
+        row.after(new_row);
     };
     
+//inserts a cell in each row of the table, including header
     this.custom_add_col = function (el) {
-		
+        debugger;
+        var table = this.getHTMLElement(el);
+        var table_native = table[0];
+        var current_cell = table.find("[data-mlab-dt-table-focus='1']");
+        var current_col = current_cell.index();
+		var num_rows = table.find("tbody tr").length;
+        
+//first insert a cell in the header
+        var new_col = $(this.config.custom.header_cell_html);
+        new_col.focus(function() {
+            $(this).closest('table').find("[data-mlab-dt-table-focus='1']").removeAttr('data-mlab-dt-table-focus');
+            $(this).attr("data-mlab-dt-table-focus", 1);
+        });
+        $(table_native.rows[0].cells[current_col]).after(new_col);
+
+//because we exclude header, and this should always be 1 row, we go up to the complete row count in th eloop
+        for (var i = 1; i <= num_rows; i++) { 
+            new_col = $(this.config.custom.cell_html);
+            new_col.focus(function() {
+                $(this).closest('table').find("[data-mlab-dt-table-focus='1']").removeAttr('data-mlab-dt-table-focus');
+                $(this).attr("data-mlab-dt-table-focus", 1);
+            });
+            $(table_native.rows[i].cells[current_col]).after(new_col);
+        }
     };
     
     this.custom_remove_row = function (el) {
-		$(el).find("[data-mlab-dt-table-focus='1']").parent().remove();
+        var table = this.getHTMLElement(el);
+		table.find("[data-mlab-dt-table-focus='1']").parent().remove();
     };
     
     this.custom_remove_col = function (el) {
